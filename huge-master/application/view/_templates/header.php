cc9494e54d1cd7d6f8c3c1458edc3d53394c13d1
iindigoo <indigobaloe@gmail.com>
2016-06-20 22:11:30 +0200

    Begin header huge

--- huge-master/application/view/_templates/header.php
+++ huge-master/application/view/_templates/header.php
@@ -1,79 +1,57 @@
-<!doctype html>
-<html>
-<head>
-    <title>HUGE</title>
-    <!-- META -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
-    <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
-    <link rel="icon" href="data:;base64,=">
-    <!-- CSS -->
-    <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
-</head>
-<body>
-    <!-- wrapper, to center website -->
-    <div class="wrapper">
+    <title>Flat UI - Free Bootstrap Framework and Theme</title>
+    <meta name="description" content="Flat UI Kit Free is a Twitter Bootstrap Framework design and Theme, this responsive framework includes a PSD and HTML version."/>
 
-        <!-- logo -->
-        <div class="logo"></div>
+    <meta name="viewport" content="width=1000, initial-scale=1.0, maximum-scale=1.0">
 
-        <!-- navigation -->
-        <ul class="navigation">
-            <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>index/index">Index</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "profile")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>profile/index">Profiles</a>
-            </li>
-            <?php if (Session::userIsLoggedIn()) { ?>
-                <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
-                </li>
-                <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
-                </li>
-            <?php } else { ?>
-                <!-- for not logged in users -->
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
-                </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-                </li>
-            <?php } ?>
-        </ul>
+    <!-- Loading Bootstrap -->
+    <link href="<?php echo Config::get('URL'); ?>css/bootstrap.css" rel="stylesheet">
 
-        <!-- my account -->
-        <ul class="navigation right">
-        <?php if (Session::userIsLoggedIn()) : ?>
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/index">My Account</a>
-                <ul class="navigation-submenu">
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/changeUserRole">Change account type</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/editAvatar">Edit your avatar</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/editusername">Edit my username</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/edituseremail">Edit my email</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/changePassword">Change Password</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>login/logout">Logout</a>
-                    </li>
-                </ul>
-            </li>
-            <?php if (Session::get("user_account_type") == 7) : ?>
-                <li <?php if (View::checkForActiveController($filename, "admin")) {
-                    echo ' class="active" ';
-                } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
-                </li>
-            <?php endif; ?>
-        <?php endif; ?>
-        </ul>
+    <!-- Loading Flat UI -->
+    <link href="<?php echo Config::get('URL'); ?>css/flat-ui.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+    
+    <!-- Loading Font Awsome -->
+    <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/font-awesome.css">
+
+    <!-- Loading Costum Flat UI -->
+     <link href="<?php echo Config::get('URL'); ?>css/style.css" rel="stylesheet">
+
+     
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="dist/js/vendor/html5shiv.js"></script>
+      <script src="dist/js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <header>
+		<nav class="navbar transparent navbar-default" role="navigation">
+			<div class="box box-nav">
+			  <div class="navbar-header">
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+			      <span class="sr-only">Toggle navigation</span>
+			    </button>
+			    <a class="navbar-brand" href="index.php">Lucky4</a>
+			  </div>
+			  <div class="collapse navbar-collapse" id="navbar-collapse-01">
+			    <ul class="nav navbar-nav">
+			      <li><a href="tickets.php">Tickets</a></li>
+            <li><a href="roullete.php">Roullete</a></li>
+			      <li><a href="putin.php">Putin</a></li>
+			    </ul>
+			    <div class="navbar-right sign">
+			    	<ul class="nav navbar-nav">
+			      	<li class="sign"><a href="login.php">Sign in</a></li>
+					<button class="btn btn-default navbar-btn btn-sm" type="button"><a href="register.php">Sign up</a></button>	
+					</ul>
+				</div>
+			  </div><!-- /.navbar-collapse -->
+			`</div>
+		</nav><!-- /navbar -->
+    </header>
--- lucky4csgo/lucky4new/login.php
+++ lucky4csgo/lucky4new/login.php
@@ -1,5 +1,6 @@
 <?php 
 	include "common/login.php";
+	include "common/header.php";
  ?>
 
 <!DOCTYPE html>
@@ -8,32 +9,22 @@
 	<meta charset="UTF-8">
 	<title>Login</title>
 	<link rel="stylesheet" href="dist/css/login.css">
-	<link rel="stylesheet" href="dist/css/bootstrap.css">
 	<script src="https://use.fontawesome.com/7a7f3effb0.js"></script>
 </head>
-<body class="color">
-<div class = "container">
-	<div class="wrapper">
-		<form action="" method="post" name="Login_Form" class="form-signin">       
-		    <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
-			  <hr class="colorgraph"><br>
-			  
-			  <input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
-			  <input type="passwordlogin" class="form-control" name="Password" placeholder="Password" required=""/>     		  
-			 
-			 
-			  <button class="btn btn-lg btn-danger btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>
-
-			  <a href="register.php"><button class="btn btn-lg btn-primary btn-block test"  name="Register" value="Register" type="button">Register</button></a>
-
-
-
-			  <div class="home">
-			  	<a href="index.php"><i class="fa fa-home fa-4x"  aria-hidden="true"></i></a>
-			  </div>
-		</form>			
-
-	</div>
+<body>
+<div id="wrapper">
+	<form autocomplete="off">
+		<div class="input-wrapper">
+			<label for="username"><i class="fa fa-user fa-2x"></i></label><input type="text" id="username" placeholder="Username" spellcheck="false">
+		</div>
+		<div class="input-wrapper">
+			<label for="password"><i class="fa fa-lock fa-2x"></i></label><input type="password" id="password" placeholder="Password">
+		</div>
+		<div class="input-wrapper">
+			<input type="submit" value="Sign In">
+		
+		</div>
+	</form>
 </div>
 </body>
 </html>
